866d8991276a93f4e9dad1baaa3b0ec35b4757a2
marufurrahman <marufurrahman1997@gmail.com>
2018-10-10 21:27:04 +0600

    initial commit

---
+++ my_app/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.views.generic import TemplateView
+class HomeView(TemplateView):
+    template_name = 'index.html'
